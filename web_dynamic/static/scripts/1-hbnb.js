c5e2868b136ac92b5bb7a222cb3bfad04e003e9b
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-24 13:21:40 -0300

    adding cosolelos

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
     const obj = {};
     $('.check').click(function() {
         $('#checkId').prop('checked', true);
+        console.log('prende')
         obj[$(this).attr('data-name')] = $(this).attr('data-id');
       });
     $('.uncheck').click(function() {
         $('#checkId').prop('checked', false);
+        console.log('apaga')
         delete obj[$(this).attr('data-name')];
       });
       const names = Object.keys(obj);
