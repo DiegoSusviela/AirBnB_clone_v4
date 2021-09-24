5a297819395f6064c3569116f2e8b49910975e73
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-24 13:37:16 -0300

    idk if this will work

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,22 +2,13 @@
 $(document).ready(function() {
     const obj = {};
     $('.amenities .popover input').click(function() {
-        alert('clickeo');
         if ($(this).porp('checked', true)) {
           alert('esta clickeado');
+          obj[$(this).attr('data-name')] = $(this).attr('data-id');
         } else { 
           alert('no esta clickeado');
+          delete obj[$(this).attr('data-name')];
         }
-
-
-
-        
-        $('#checkId').prop('checked', true);
-        obj[$(this).attr('data-name')] = $(this).attr('data-id');
-      });
-    $('.uncheck').click(function() {
-        $('#checkId').prop('checked', false);
-        delete obj[$(this).attr('data-name')];
       });
       const names = Object.keys(obj);
       $('.amenities h4').text(names.sort().join(', '));
