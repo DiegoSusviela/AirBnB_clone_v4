2c7b371502f31ea74e133f3610ec1cb99c727a4c
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-24 13:38:19 -0300

    testng with no prop

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
     const obj = {};
     $('.amenities .popover input').click(function() {
-        if ($(this).porp('checked', true)) {
+        if (this.checked) {
           alert('esta clickeado');
           obj[$(this).attr('data-name')] = $(this).attr('data-id');
         } else { 
