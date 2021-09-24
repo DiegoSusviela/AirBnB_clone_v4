4b728dd99528aabe0fa9371042bb2d9b188cdb84
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-24 13:26:30 -0300

    added an alert at start

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
     const obj = {};
+    alert('entro');
     $('.check').click(function() {
+        alert('prende');
         $('#checkId').prop('checked', true);
-        alert('prende')
+
         obj[$(this).attr('data-name')] = $(this).attr('data-id');
       });
     $('.uncheck').click(function() {
+        alert('apaga');
         $('#checkId').prop('checked', false);
-        alert('apaga')
         delete obj[$(this).attr('data-name')];
       });
       const names = Object.keys(obj);
