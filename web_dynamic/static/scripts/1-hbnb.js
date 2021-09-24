73e8c63a509571253d5aad832f3f4767a238ff0f
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-24 12:28:51 -0300

    chanign js script

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,28 +1,13 @@
-$(document).ready(function () {
-  const obj = {};
-  $('.amenities .popover input').change(function () {
-    if ($(this).porp('checked', true)) {
-      obj[$(this).attr('data-name')] = $(this).attr('data-id');
-    } else if ($(this).prop('checked', false)) {
-      delete obj[$(this).attr('data-name')];
-    }
-    const names = Object.keys(obj);
-    $('.amenities h4').text(names.sort().join(', '));
-  });
-});
-
-
 $(document).ready(function() {
     const obj = {};
-    $(".check").click(function() {
-        $("#checkId").prop("checked", true);
+    $('.check').click(function() {
+        $('#checkId').prop('checked', true);
         obj[$(this).attr('data-name')] = $(this).attr('data-id');
       });
-    $(".uncheck").click(function() {
-        $("#checkId").prop("checked", false);
+    $('.uncheck').click(function() {
+        $('#checkId').prop('checked', false);
         delete obj[$(this).attr('data-name')];
       });
       const names = Object.keys(obj);
       $('.amenities h4').text(names.sort().join(', '));
   });
-  
