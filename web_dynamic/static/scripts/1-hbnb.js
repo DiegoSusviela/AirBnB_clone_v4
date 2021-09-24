5316388c271b3b741e9aa9a5a567df098ef87062
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-24 13:44:20 -0300

    changed to values

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
     const obj = {};
     $('.amenities .popover input').click(function() {
         if (this.checked) {
-          alert('esta clickeado');
           obj[$(this).attr('data-name')] = $(this).attr('data-id');
+          alert($(this).attr('data-name'));
         } else { 
           alert('no esta clickeado');
           delete obj[$(this).attr('data-name')];
