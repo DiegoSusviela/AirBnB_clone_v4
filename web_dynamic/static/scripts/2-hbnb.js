86928492f8c8687b6cfc61d52de6a8078d48f579
toshi-uy <s_borgia@hotmail.com>
2021-09-25 17:04:00 -0300

    added flex direction in css and 127 instead of 0.0.0.0

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $.get('http://0.0.0.0:5001/api/v1/status/', data => {
+  $.get('http://127.0.0.1:5001/api/v1/status/', data => {
     if (data.status === 'OK') {
       $('div#api_status').addClass('available');
     } else {
