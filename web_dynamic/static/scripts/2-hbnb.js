0d3dea8b354d853b9c7e09d3cefad852a649c5a1
toshi-uy <s_borgia@hotmail.com>
2021-09-25 16:55:39 -0300

    testing status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $.get('http://0.0.0.0:5001/api/v1/status/', data => {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  })
+});
+
+
+$(document).ready(function() {
+    const obj = {};
+    $('.amenities .popover input').click(function() {
+        if (this.checked) {
+          obj[$(this).data('id')] = $(this).data('name');
+        } else {
+          delete obj[$(this).data('id')];
+        }
+        const names = Object.values(obj);
+        $('.amenities h4').text(names.sort().join(', '));
+      });
+  });
