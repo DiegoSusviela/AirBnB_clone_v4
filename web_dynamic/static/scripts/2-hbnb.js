93d313198e26ce0a34ff0412145b3072e426ac7a
toshi-uy <s_borgia@hotmail.com>
2021-09-25 17:07:48 -0300

    removed div

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $.getJSON('http://0.0.0.0:5001/api/v1/status/', data => {
     if (data.status === 'OK') {
-      $('div#api_status').addClass('available');
+      $('#api_status').addClass('available');
     } else {
-      $('div#api_status').removeClass('available');
+      $('#api_status').removeClass('available');
     }
   })
 });
