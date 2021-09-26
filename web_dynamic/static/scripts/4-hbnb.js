c67c8f02d977d1962066380712f22e567c5f085f
toshi-uy <s_borgia@hotmail.com>
2021-09-26 18:03:56 -0300

    included all in document. function

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -8,10 +8,7 @@ $(document).ready(function() {
       }
     }
   })
-});
-
-
-$(document).ready(function() {
+  $(document).ready(function() {
     const obj = {};
     $('.amenities .popover input').click(function() {
         if (this.checked) {
@@ -24,37 +21,38 @@ $(document).ready(function() {
       });
   });
 
-$.ajax({
-  type: 'POST',
-  url: 'http://' + location.hostname + ':5001/api/v1/places_search/',
-  contentType: 'application/json',
-  data: '{}',
-  success: function (data) {
-    $.each(data, function (i, place) {
-      $('section.places').append('<article><div class="title_box"><h2>'+ place.name +'</h2><div class="price_by_night">$'+ place.price_by_night +'</div></div><div class="information"><div class="max_guest">'+ place.max_guest +' Guest' + (place.max_guest != 1 ? 's': '') + '</div><div class="number_rooms">'+ place.number_rooms +' Bedroom' + (place.number_rooms != 1 ? 's': '') + '</div><div class="number_bathrooms">'+ place.number_bathrooms +' Bathroom' + (place.number_bathrooms != 1 ? 's': '') + '</div></div><div class="description">'+ place.description +'</div></article>');
-    });
-  }
-});
-
-
-$(function () {
-  $('button').click(function (event) {
-    alert('botton pressed');
-    $.ajax({
-	  type: 'POST',
+  $.ajax({
+    type: 'POST',
     url: 'http://' + location.hostname + ':5001/api/v1/places_search/',
     contentType: 'application/json',
-    dataType: 'JSON',
-    data: JSON.stringify({ amenities: Object.keys(obj) }),
+    data: '{}',
     success: function (data) {
-      $('section.places > article').remove();
-      let ret = [];
       $.each(data, function (i, place) {
-        ret.push(createHTML(data[i]));
+        $('section.places').append('<article><div class="title_box"><h2>'+ place.name +'</h2><div class="price_by_night">$'+ place.price_by_night +'</div></div><div class="information"><div class="max_guest">'+ place.max_guest +' Guest' + (place.max_guest != 1 ? 's': '') + '</div><div class="number_rooms">'+ place.number_rooms +' Bedroom' + (place.number_rooms != 1 ? 's': '') + '</div><div class="number_bathrooms">'+ place.number_bathrooms +' Bathroom' + (place.number_bathrooms != 1 ? 's': '') + '</div></div><div class="description">'+ place.description +'</div></article>');
       });
-      ret = ret.join('');
-      $('section.places').append(ret);
-      }
+    }
+  });
+
+
+  $(function () {
+    $('button').click(function (event) {
+      alert('botton pressed');
+      $.ajax({
+      type: 'POST',
+      url: 'http://' + location.hostname + ':5001/api/v1/places_search/',
+      contentType: 'application/json',
+      dataType: 'JSON',
+      data: JSON.stringify({ amenities: Object.keys(obj) }),
+      success: function (data) {
+        $('section.places > article').remove();
+        let ret = [];
+        $.each(data, function (i, place) {
+          ret.push(createHTML(data[i]));
+        });
+        ret = ret.join('');
+        $('section.places').append(ret);
+        }
+      });
     });
   });
 });
