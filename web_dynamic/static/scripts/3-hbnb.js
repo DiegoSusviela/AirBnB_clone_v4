fb924f4e63abf9eb2b9d0508cc5d91a9cc408d52
toshi-uy <s_borgia@hotmail.com>
2021-09-26 18:45:19 -0300

    changed styling errors

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -9,9 +9,7 @@ $(document).ready(function () {
       }
     }
   });
-});
 
-$(document).ready(function () {
   const obj = {};
   $('.amenities .popover input').click(function () {
     if (this.checked) {
@@ -22,16 +20,16 @@ $(document).ready(function () {
     const names = Object.values(obj);
     $('.amenities h4').text(names.sort().join(', '));
   });
-});
-
-$.ajax({
-  type: 'POST',
-  url: 'http://' + location.hostname + ':5001/api/v1/places_search/',
-  contentType: 'application/json',
-  data: '{}',
-  success: function (data) {
-    $.each(data, function (i, place) {
-      $('section.places').append('<article><div class="title_box"><h2>'+ place.name +'</h2><div class="price_by_night">$'+ place.price_by_night +'</div></div><div class="information"><div class="max_guest">'+ place.max_guest +' Guest' + (place.max_guest != 1 ? 's': '') + '</div><div class="number_rooms">'+ place.number_rooms +' Bedroom' + (place.number_rooms != 1 ? 's': '') + '</div><div class="number_bathrooms">'+ place.number_bathrooms +' Bathroom' + (place.number_bathrooms != 1 ? 's': '') + '</div></div><div class="description">'+ place.description +'</div></article>');
-    });
-  }
+  
+  $.ajax({
+    type: 'POST',
+    url: 'http://' + hostname + ':5001/api/v1/places_search/',
+    contentType: 'application/json',
+    data: '{}',
+    success: function (data) {
+      $.each(data, function (i, place) {
+        $('section.places').append('<article><div class="title_box"><h2>' + place.name + '</h2><div class="price_by_night">$' + place.price_by_night + '</div></div><div class="information"><div class="max_guest">' + place.max_guest + ' Guest' + (place.max_guest != 1 ? 's': '') + '</div><div class="number_rooms">' + place.number_rooms + ' Bedroom' + (place.number_rooms != 1 ? 's': '') + '</div><div class="number_bathrooms">' + place.number_bathrooms + ' Bathroom' + (place.number_bathrooms != 1 ? 's': '') + '</div></div><div class="description">' + place.description + '</div></article>');
+      });
+    }
+  });  
 });
