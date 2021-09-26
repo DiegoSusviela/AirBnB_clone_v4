298bdb907d28b5a9f73dfebf54e58b7344e89683
toshi-uy <s_borgia@hotmail.com>
2021-09-26 18:42:11 -0300

    changed styling errors

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,5 +1,6 @@
-$(document).ready(function() {
-  $.get('http://' + location.hostname + ':5001/api/v1/status/', function (data, status) {
+$(document).ready(function () {
+  const hostname = window.location.hostname;
+  $.get('http://' + hostname + ':5001/api/v1/status/', function (data, status) {
     if (status === 'success') {
       if (data.status === 'OK') {
         $('DIV#api_status').addClass('available');
@@ -7,22 +8,21 @@ $(document).ready(function() {
         $('DIV#api_status').removeClass('available');
       }
     }
-  })
+  });
 });
 
-
-$(document).ready(function() {
-    const obj = {};
-    $('.amenities .popover input').click(function() {
-        if (this.checked) {
-          obj[$(this).data('id')] = $(this).data('name');
-        } else {
-          delete obj[$(this).data('id')];
-        }
-        const names = Object.values(obj);
-        $('.amenities h4').text(names.sort().join(', '));
-      });
+$(document).ready(function () {
+  const obj = {};
+  $('.amenities .popover input').click(function () {
+    if (this.checked) {
+      obj[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete obj[$(this).data('id')];
+    }
+    const names = Object.values(obj);
+    $('.amenities h4').text(names.sort().join(', '));
   });
+});
 
 $.ajax({
   type: 'POST',
