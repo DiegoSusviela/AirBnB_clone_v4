c727bfa713e93f8989f24c27d2f9b8dddc8e049b
toshi-uy <s_borgia@hotmail.com>
2021-09-26 18:53:17 -0300

    changed styling errors

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
-  const obj = {};
-  $.get('http://' + location.hostname + ':5001/api/v1/status/', function (data, status) {
+$(document).ready(function () {
+  const hostname = window.location.hostname;
+  $.get('http://' + hostname + ':5001/api/v1/status/', function (data, status) {
     if (status === 'success') {
       if (data.status === 'OK') {
         $('DIV#api_status').addClass('available');
@@ -8,31 +8,30 @@ $(document).ready(function() {
         $('DIV#api_status').removeClass('available');
       }
     }
-  })
-  $(document).ready(function() {
-    $('.amenities .popover input').click(function() {
-        if (this.checked) {
-          obj[$(this).data('id')] = $(this).data('name');
-        } else {
-          delete obj[$(this).data('id')];
-        }
-        const names = Object.values(obj);
-        $('.amenities h4').text(names.sort().join(', '));
-      });
+  });
+
+  const obj = {};
+  $('.amenities .popover input').click(function () {
+    if (this.checked) {
+      obj[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete obj[$(this).data('id')];
+    }
+    const names = Object.values(obj);
+    $('.amenities h4').text(names.sort().join(', '));
   });
 
   $.ajax({
     type: 'POST',
-    url: 'http://' + location.hostname + ':5001/api/v1/places_search/',
+    url: 'http://' + hostname + ':5001/api/v1/places_search/',
     contentType: 'application/json',
     data: '{}',
     success: function (data) {
       $.each(data, function (i, place) {
-        $('section.places').append('<article><div class="title_box"><h2>'+ place.name +'</h2><div class="price_by_night">$'+ place.price_by_night +'</div></div><div class="information"><div class="max_guest">'+ place.max_guest +' Guest' + (place.max_guest != 1 ? 's': '') + '</div><div class="number_rooms">'+ place.number_rooms +' Bedroom' + (place.number_rooms != 1 ? 's': '') + '</div><div class="number_bathrooms">'+ place.number_bathrooms +' Bathroom' + (place.number_bathrooms != 1 ? 's': '') + '</div></div><div class="description">'+ place.description +'</div></article>');
+        $('section.places').append('<article><div class="title_box"><h2>' + place.name + '</h2><div class="price_by_night">$' + place.price_by_night + '</div></div><div class="information"><div class="max_guest">' + place.max_guest + ' Guest' + (place.max_guest !== 1 ? 's' : '') + '</div><div class="number_rooms">' + place.number_rooms + ' Bedroom' + (place.number_rooms !== 1 ? 's' : '') + '</div><div class="number_bathrooms">' + place.number_bathrooms + ' Bathroom' + (place.number_bathrooms !== 1 ? 's' : '') + '</div></div><div class="description">' + place.description + '</div></article>');
       });
     }
   });
-
 
   $(function () {
     $('button').click(function (event) {
@@ -46,7 +45,7 @@ $(document).ready(function() {
       success: function (data) {
         $('section.places').empty();
         for (const place of data) {
-          $('section.places').append('<article><div class="title_box"><h2>'+ place.name +'</h2><div class="price_by_night">$'+ place.price_by_night +'</div></div><div class="information"><div class="max_guest">'+ place.max_guest +' Guest' + (place.max_guest != 1 ? 's': '') + '</div><div class="number_rooms">'+ place.number_rooms +' Bedroom' + (place.number_rooms != 1 ? 's': '') + '</div><div class="number_bathrooms">'+ place.number_bathrooms +' Bathroom' + (place.number_bathrooms != 1 ? 's': '') + '</div></div><div class="description">'+ place.description +'</div></article>');
+          $('section.places').append('<article><div class="title_box"><h2>' + place.name + '</h2><div class="price_by_night">$' + place.price_by_night + '</div></div><div class="information"><div class="max_guest">' + place.max_guest + ' Guest' + (place.max_guest !== 1 ? 's' : '') + '</div><div class="number_rooms">' + place.number_rooms + ' Bedroom' + (place.number_rooms !== 1 ? 's' : '') + '</div><div class="number_bathrooms">' + place.number_bathrooms + ' Bathroom' + (place.number_bathrooms !== 1 ? 's' : '') + '</div></div><div class="description">' + place.description + '</div></article>');
         }
       }
       });
