72fb58c43e59d211908558d02d2e474787adf903
toshi-uy <s_borgia@hotmail.com>
2021-09-26 18:12:52 -0300

    added for

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -44,10 +44,12 @@ $(document).ready(function() {
       dataType: 'JSON',
       data: JSON.stringify({ amenities: Object.keys(obj) }),
       success: function (data) {
-        $('section.places > article').remove();
-        $.each(data, function (i, place) {
-          $('section.places').append('<article><div class="title_box"><h2>'+ place.name +'</h2><div class="price_by_night">$'+ place.price_by_night +'</div></div><div class="information"><div class="max_guest">'+ place.max_guest +' Guest' + (place.max_guest != 1 ? 's': '') + '</div><div class="number_rooms">'+ place.number_rooms +' Bedroom' + (place.number_rooms != 1 ? 's': '') + '</div><div class="number_bathrooms">'+ place.number_bathrooms +' Bathroom' + (place.number_bathrooms != 1 ? 's': '') + '</div></div><div class="description">'+ place.description +'</div></article>');
-        });
+        $('section.places').empty();
+        for (const place of data) {
+          $.each(data, function (i, place) {
+            $('section.places').append('<article><div class="title_box"><h2>'+ place.name +'</h2><div class="price_by_night">$'+ place.price_by_night +'</div></div><div class="information"><div class="max_guest">'+ place.max_guest +' Guest' + (place.max_guest != 1 ? 's': '') + '</div><div class="number_rooms">'+ place.number_rooms +' Bedroom' + (place.number_rooms != 1 ? 's': '') + '</div><div class="number_bathrooms">'+ place.number_bathrooms +' Bathroom' + (place.number_bathrooms != 1 ? 's': '') + '</div></div><div class="description">'+ place.description +'</div></article>');
+          });
+        }
       }
       });
     });
