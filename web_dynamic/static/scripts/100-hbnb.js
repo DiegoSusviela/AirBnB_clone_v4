07c8172ca62719c4a10785fc42f8ac09eff5ce3b
toshi-uy <s_borgia@hotmail.com>
2021-09-26 20:26:28 -0300

    alerts to debug

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -26,7 +26,7 @@ $(document).ready(function () {
   const states = {};
   $('.locations .popover ul li h2 input').click(function () {
     if (this.checked) {
-      alert($(this).data('state_id'));
+      alert($(this).data('name'));
       states[$(this).data('id')] = $(this).data('name');
     } else {
       delete states[$(this).data('id')];
