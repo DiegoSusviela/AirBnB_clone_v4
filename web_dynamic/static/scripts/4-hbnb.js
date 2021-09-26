c4499e07917b8e23ccfd4d514c6f141af880fa11
toshi-uy <s_borgia@hotmail.com>
2021-09-26 18:09:12 -0300

    testing places

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -45,13 +45,10 @@ $(document).ready(function() {
       data: JSON.stringify({ amenities: Object.keys(obj) }),
       success: function (data) {
         $('section.places > article').remove();
-        let ret = [];
         $.each(data, function (i, place) {
-          ret.push(createHTML(data[i]));
+          $('section.places').append('<article><div class="title_box"><h2>'+ place.name +'</h2><div class="price_by_night">$'+ place.price_by_night +'</div></div><div class="information"><div class="max_guest">'+ place.max_guest +' Guest' + (place.max_guest != 1 ? 's': '') + '</div><div class="number_rooms">'+ place.number_rooms +' Bedroom' + (place.number_rooms != 1 ? 's': '') + '</div><div class="number_bathrooms">'+ place.number_bathrooms +' Bathroom' + (place.number_bathrooms != 1 ? 's': '') + '</div></div><div class="description">'+ place.description +'</div></article>');
         });
-        ret = ret.join('');
-        $('section.places').append(ret);
-        }
+      }
       });
     });
   });
