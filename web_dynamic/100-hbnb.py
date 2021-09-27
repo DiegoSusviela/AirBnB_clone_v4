f1429fbfbe90e829a22f20289b0922dfafc1ebde
toshi-uy <s_borgia@hotmail.com>
2021-09-27 10:12:14 -0300

    changed back to state[0]

--- web_dynamic/100-hbnb.py
+++ web_dynamic/100-hbnb.py
@@ -34,7 +34,7 @@ def hbnb():
     places = sorted(places, key=lambda k: k.name)
 
     return render_template('100-hbnb.html',
-                           states=states,
+                           states=st_ct,
                            amenities=amenities,
                            places=places,
                            cache_id=uuid.uuid4())
