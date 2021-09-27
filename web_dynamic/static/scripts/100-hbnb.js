83013c2cf7ca1a16262131ad013b542bc3d50469
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-27 10:40:12 -0300

    testing objects.keys

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -71,7 +71,7 @@ $(document).ready(function () {
         url: 'http://' + hostname + ':5001/api/v1/places_search/',
         contentType: 'application/json',
         dataType: 'JSON',
-        data: JSON.stringify({ amenities: Object.keys(amenities) }),
+        data: JSON.stringify({ amenities: Object.keys(amenities, statesCities) }),
         success: function (data) {
           $('section.places').empty();
           for (const place of data) {
