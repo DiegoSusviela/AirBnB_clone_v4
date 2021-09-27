ea34ee0b0d1a3f770448b074b85d791f1635486e
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-27 10:52:04 -0300

    stinging object

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -73,7 +73,7 @@ $(document).ready(function () {
         dataType: 'JSON',
         data: JSON.stringify({ amenities: Object.keys(statesCities) }),
         success: function (data) {
-          alert(statesCities);
+          alert(string(statesCities));
           $('section.places').empty();
           alert(data);
           for (const place of data) {
