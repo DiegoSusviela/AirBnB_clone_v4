e8b4281486b5b21542b4112d3ea0d5dc33a8fe64
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-27 11:02:00 -0300

    removed alerts

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -79,9 +79,7 @@ $(document).ready(function () {
         dataType: 'JSON',
         data: JSON.stringify({ states: Object.keys(states), amenities: Object.keys(amenities), cities: Object.keys(cities)}),
         success: function (data) {
-          alert(String(statesCities));
           $('section.places').empty();
-          alert(data);
           for (const place of data) {
             $('section.places').append('<article><div class="title_box"><h2>' + place.name + '</h2><div class="price_by_night">$' + place.price_by_night + '</div></div><div class="information"><div class="max_guest">' + place.max_guest + ' Guest' + (place.max_guest !== 1 ? 's' : '') + '</div><div class="number_rooms">' + place.number_rooms + ' Bedroom' + (place.number_rooms !== 1 ? 's' : '') + '</div><div class="number_bathrooms">' + place.number_bathrooms + ' Bathroom' + (place.number_bathrooms !== 1 ? 's' : '') + '</div></div><div class="description">' + place.description + '</div></article>');
           }
